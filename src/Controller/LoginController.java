208ef16e42353935d23602f76b92c7505f6d4479
Saxaar <89688511251aA>
2021-03-03 12:57:02 +0300

    Initial commit (ToDoApp)

---
+++ src/Controller/LoginController.java
@@ -0,0 +1,34 @@
+package Controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button loginSignUpButton;
+
+    @FXML
+    private TextField loginUsername;
+
+    @FXML
+    private PasswordField loginPassword;
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    void initialize() {
+
+
+    }
+}
